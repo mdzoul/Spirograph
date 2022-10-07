e05a904bbf1dec2a0a26bb5241b245efe333e6bb
Zoul Aimi <82052598+mdzoul@users.noreply.github.com>
2022-10-08 02:47:41 +0800

    Coded other polygonal spirographs

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# Basic Circle Spirograph
 import turtle
 from turtle import Turtle, Screen
 import random
@@ -13,18 +12,32 @@ def random_color():
     return r, g, b
 
 
-def draw_spirograph(gap_size):
+def draw_polygon_spirograph(gap_size, sides):
     tim.hideturtle()
     for _ in range(int(360 / gap_size)):
         tim.color(random_color())
-        tim.circle(100)
+        for _ in range(sides):
+            tim.fd(100)
+            tim.rt(int(360 / sides))
+        tim.left(gap_size)
+
+
+def draw_circle_spirograph(gap_size):
+    tim.hideturtle()
+    for _ in range(int(360 / gap_size)):
+        tim.color(random_color())
+        tim.circle(300)
         tim.left(gap_size)
 
 
 tim = Turtle()
 tim.speed(0)
 
-draw_spirograph(5)
+for num_sides in range(3, 11):
+    draw_polygon_spirograph(5, num_sides)
+    tim.clear()
+
+draw_circle_spirograph(5)
 
 screen = Screen()
 screen.exitonclick()
